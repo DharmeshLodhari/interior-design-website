411424f927a76f24b5b3613ab1e70c0331081e6e
Dharmesh <dishapatel.srashtasoft@gmail.com>
2026-02-24 11:50:39 +0530

    Fix GitHub Pages routing with HashRouter and add 404 handling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Portfolio from './Portfolio';
 import ProjectDetail from './ProjectDetail';
